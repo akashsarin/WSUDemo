879e023a8c6a3dc417063555098477736b9361f6
akashsarin <61521115+akashsarin@users.noreply.github.com>
2023-09-03 13:15:25 -0400

    no need to add explicitly

--- demo_script_1.py
+++ demo_script_1.py
@@ -1,1 +1,7 @@
 print("Hello World!")
+
+# Demo some changes
+print("This is my first change after my first commit.")
+
+# Demo -am
+print("I can use -am on commit and not add and commit separately")
