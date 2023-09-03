d8a7e45bdcc157dabe09634c9395145b2615733d
Akash Sarin <61521115+akashsarin@users.noreply.github.com>
2023-09-03 13:19:12 -0400

    Update demo_script_1.py

--- demo_script_1.py
+++ demo_script_1.py
@@ -5,3 +5,6 @@ print("This is my first change after my first commit.")
 
 # Demo -am
 print("I can use -am on commit and not add and commit separately")
+
+# Demo pull
+print("Changes made by someone else")
